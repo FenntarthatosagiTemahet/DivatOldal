c6c535529cc3233392aedc2f4d4665eb39f574fb
WeinbergerPeti <weinbergerpeti@gmail.com>
2022-04-20 13:52:00 +0200

    alap kiindulás

---
+++ scriptek/jatek.js
@@ -0,0 +1,26 @@
+window.addEventListener("load", init);
+
+function ID(elem)
+{
+    return document.getElementById(elem);
+}
+
+function $(elem)
+{
+    return document.querySelector(elem);
+}
+
+function $All(elem)
+{
+    return document.querySelectorAll(elem);
+}
+
+function CLASS(elem)
+{
+    return document.getElementsByClassName(elem);
+}
+
+function init() 
+{
+    
+}

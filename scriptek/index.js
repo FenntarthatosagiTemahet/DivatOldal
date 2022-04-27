30a6fb3e1648e5ee54f3bb89623d3870437f547f
panel05 <pantlbalazs@gmail.com>
2022-04-27 12:53:52 +0200

    landing page

--- scriptek/index.js
+++ scriptek/index.js
@@ -1,26 +1,50 @@
-window.addEventListener("load", init);
+// window.addEventListener("load", init);
 
-function ID(elem)
-{
-    return document.getElementById(elem);
-}
+// function ID(elem) {
+//   return document.getElementById(elem);
+// }
 
-function $(elem)
-{
-    return document.querySelector(elem);
-}
+// function $(elem) {
+//   return document.querySelector(elem);
+// }
 
-function $All(elem)
-{
-    return document.querySelectorAll(elem);
-}
+// function $All(elem) {
+//   return document.querySelectorAll(elem);
+// }
 
-function CLASS(elem)
-{
-    return document.getElementsByClassName(elem);
-}
+// function CLASS(elem) {
+//   return document.getElementsByClassName(elem);
+// }
 
-function init() 
-{
-    console.log("Szia");
-}
+// const manipul = [
+//   {
+//     szoveg: "lorem",
+//     kep: "../kepek/facepalm.jpg",
+//     link: "cikk.html",
+//   },
+//   {
+//     szoveg: "lorem",
+//     kep: "kepeleresi_ut",
+//     link: "galeria.html",
+//   },
+//   {
+//     szoveg: "lorem",
+//     kep: "kepeleresi_ut",
+//     link: "jatek.html",
+//   },
+//   {
+//     szoveg: "lorem",
+//     kep: "kepeleresi_ut",
+//     link: "teszt.html",
+//   } 
+// ]
+
+// function init() {
+//   let txt = "<div id ='valami'>";
+//   for (let index = 0; index < manipul.length; index++) {
+//       txt += `<div>${manipul[index].szoveg}</div><div><a href="${manipul[index].link}><img src="${manipul[index].kep}"/></a></div>`; 
+//       console.log(txt)
+//   }
+//   txt += "</div>"
+//   ID("fodiv").innerHTML= txt;
+// }

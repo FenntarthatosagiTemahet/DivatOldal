f319c9a1321b70990d74c7c0e6ad4aa7865952d7
panel05 <pantlbalazs@gmail.com>
2022-05-03 16:23:40 +0200

    index_css_javaAlap

--- scriptek/index.js
+++ scriptek/index.js
@@ -1,50 +1,55 @@
-// window.addEventListener("load", init);
+window.addEventListener("load", init);
 
-// function ID(elem) {
-//   return document.getElementById(elem);
-// }
+function ID(elem) {
+  return document.getElementById(elem);
+}
 
-// function $(elem) {
-//   return document.querySelector(elem);
-// }
+function $(elem) {
+  return document.querySelector(elem);
+}
 
-// function $All(elem) {
-//   return document.querySelectorAll(elem);
-// }
+function $All(elem) {
+  return document.querySelectorAll(elem);
+}
 
-// function CLASS(elem) {
-//   return document.getElementsByClassName(elem);
-// }
+function CLASS(elem) {
+  return document.getElementsByClassName(elem);
+}
 
-// const manipul = [
-//   {
-//     szoveg: "lorem",
-//     kep: "../kepek/facepalm.jpg",
-//     link: "cikk.html",
-//   },
-//   {
-//     szoveg: "lorem",
-//     kep: "kepeleresi_ut",
-//     link: "galeria.html",
-//   },
-//   {
-//     szoveg: "lorem",
-//     kep: "kepeleresi_ut",
-//     link: "jatek.html",
-//   },
-//   {
-//     szoveg: "lorem",
-//     kep: "kepeleresi_ut",
-//     link: "teszt.html",
-//   } 
-// ]
+const manipul = [
+  {
+    szoveg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed rutrum tellus. Vivamus lectus nisi.",
+    kep: "../kepek/ruha1.jpg",
+    link: "cikk.html",
+  },
+  {
+    szoveg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed rutrum tellus. Vivamus lectus nisi.",
+    kep: "../kepek/ruha2.jpg",
+    link: "galeria.html",
+  },
+  {
+    szoveg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed rutrum tellus. Vivamus lectus nisi.",
+    kep: "../kepek/ruha3.jpg",
+    link: "jatek.html",
+  },
+  {
+    szoveg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed rutrum tellus. Vivamus lectus nisi.",
+    kep: "../kepek/ruha4.jpg",
+    link: "teszt.html",
+  } 
+]
 
-// function init() {
-//   let txt = "<div id ='valami'>";
-//   for (let index = 0; index < manipul.length; index++) {
-//       txt += `<div>${manipul[index].szoveg}</div><div><a href="${manipul[index].link}><img src="${manipul[index].kep}"/></a></div>`; 
-//       console.log(txt)
-//   }
-//   txt += "</div>"
-//   ID("fodiv").innerHTML= txt;
-// }
+function init() {
+  let txt = "";
+  for (let index = 0; index < manipul.length; index++) {
+      //txt += `<div id="mellék_div"><div><p>${manipul[index].szoveg}</p></div><div><a href=${manipul[index].link}><img src="${manipul[index].kep}"alt="facepalm""/></a></div></div>`;
+      //if(index%2===0){
+        //txt += `<div id="mellék_div"><div><p>${manipul[index].szoveg}</p></div><div><a href=${manipul[index].link}><img src="${manipul[index].kep}"alt="facepalm""/></a></div></div>`;
+      //}
+      //else{
+          txt +=  `<div id="mellék_div"><div><a href=${manipul[index].link}><img src="${manipul[index].kep}"alt="facepalm""/></a></div><div><p>${manipul[index].szoveg}</p></div></div>`;
+      // }
+      console.log(txt)
+  }
+  ID("fodiv").innerHTML= txt;
+}

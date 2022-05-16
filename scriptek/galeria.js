79349591e6e0b1323d12a3eb6f1049baabebd8d1
WeinbergerPeti <weinbergerpeti@gmail.com>
2022-05-16 13:22:50 +0200

    .

--- scriptek/galeria.js
+++ scriptek/galeria.js
@@ -69,8 +69,8 @@ function kepekFeltoltAdatokkal(tomb, txt)
   // txt="";
   // $(".ing").innerHTML=txt;
   // $(".tervezok").innerHTML=asideTxt;
-  console.log(zeroTxt);
-  console.log(ingTxt);
+   console.log(zeroTxt);
+   console.log(ingTxt);
 }
 
 
@@ -90,318 +90,318 @@ function mintaKepFeltolt()
   }
 }
 
-const galeriaAdatok = [
-  {
-    kepCim: "kep cím",
-    eleres: "../kepek/lanyokKepei/",
-    keszito: "Árokszállási Renáta Andrea",
-    tipus: "női",
-    leírás: "mi alapján tervezte meg",
-    tovabbiKepek: [
-      {
-        eleres: "kep2.jpg",
-      },
-    ],
-    ar: "Nem eladó",
-    modellNeve: "XY",
-    ruhaoldalEleresiut: "",
-  },
-  {
-    kepCim: "kep cím",
-    eleres: "kep.jpg",
-    keszito: "Bajusz Gyöngyike",
-    tipus: "férfi/női",
-    leírás: "mi alapján tervezte meg",
-    tovabbiKepek: [
-      {
-        eleres: "kep2.jpg",
-      },
-    ],
-    ar: "Nem eladó",
-    modellNeve: "XY",
-    ruhaoldalEleresiut: "",
-  },
-  {
-    kepCim: "kep cím",
-    eleres: "kep.jpg",
-    keszito: "Balla Robin",
-    tipus: "férfi/női",
-    leírás: "mi alapján tervezte meg",
-    tovabbiKepek: [
-      {
-        eleres: "kep2.jpg",
-      },
-    ],
-    ar: "Nem eladó",
-    modellNeve: "XY",
-    ruhaoldalEleresiut: "",
-  },
-  {
-    kepCim: "kep cím",
-    eleres: "kep.jpg",
-    keszito: "Bartha Emma Zsuzsanna",
-    tipus: "férfi/női",
-    leírás: "mi alapján tervezte meg",
-    tovabbiKepek: [
-      {
-        eleres: "kep2.jpg",
-      },
-    ],
-    ar: "Nem eladó",
-    modellNeve: "XY",
-    ruhaoldalEleresiut: "",
-  },
-  {
-    kepCim: "kep cím",
-    eleres: "kep.jpg",
-    keszito: "Berecz Lili Blanka",
-    tipus: "férfi/női",
-    leírás: "mi alapján tervezte meg",
-    tovabbiKepek: [
-      {
-        eleres: "kep2.jpg",
-      },
-    ],
-    ar: "Nem eladó",
-    modellNeve: "XY",
-    ruhaoldalEleresiut: "",
-  },
-  {
-    kepCim: "kep cím",
-    eleres: "kep.jpg",
-    keszito: "Buzás Vanda Szonja",
-    tipus: "férfi/női",
-    leírás: "mi alapján tervezte meg",
-    tovabbiKepek: [
-      {
-        eleres: "kep2.jpg",
-      },
-    ],
-    ar: "Nem eladó",
-    modellNeve: "XY",
-    ruhaoldalEleresiut: "",
-  },
-  {
-    kepCim: "kep cím",
-    eleres: "kep.jpg",
-    keszito: "Fábián Fanni",
-    tipus: "férfi/női",
-    leírás: "mi alapján tervezte meg",
-    tovabbiKepek: [
-      {
-        eleres: "kep2.jpg",
-      },
-    ],
-    ar: "Nem eladó",
-    modellNeve: "XY",
-    ruhaoldalEleresiut: "",
-  },
-  {
-    kepCim: "kep cím",
-    eleres: "kep.jpg",
-    keszito: "Fazekas Anna",
-    tipus: "férfi/női",
-    leírás: "mi alapján tervezte meg",
-    tovabbiKepek: [
-      {
-        eleres: "kep2.jpg",
-      },
-    ],
-    ar: "Nem eladó",
-    modellNeve: "XY",
-    ruhaoldalEleresiut: "",
-  },
-  {
-    kepCim: "kep cím",
-    eleres: "kep.jpg",
-    keszito: "Fódi Martin Levente",
-    tipus: "férfi/női",
-    leírás: "mi alapján tervezte meg",
-    tovabbiKepek: [
-      {
-        eleres: "kep2.jpg",
-      },
-    ],
-    ar: "Nem eladó",
-    modellNeve: "XY",
-    ruhaoldalEleresiut: "",
-  },
-  {
-    kepCim: "kep cím",
-    eleres: "kep.jpg",
-    keszito: "Hoffmann Dóra",
-    tipus: "férfi/női",
-    leírás: "mi alapján tervezte meg",
-    tovabbiKepek: [
-      {
-        eleres: "kep2.jpg",
-      },
-    ],
-    ar: "Nem eladó",
-    modellNeve: "XY",
-    ruhaoldalEleresiut: "",
-  },
-  {
-    kepCim: "kep cím",
-    eleres: "kep.jpg",
-    keszito: "Juhász Nóra",
-    tipus: "férfi/női",
-    leírás: "mi alapján tervezte meg",
-    tovabbiKepek: [
-      {
-        eleres: "kep2.jpg",
-      },
-    ],
-    ar: "Nem eladó",
-    modellNeve: "XY",
-    ruhaoldalEleresiut: "",
-  },
-  {
-    kepCim: "kep cím",
-    eleres: "kep.jpg",
-    keszito: "Kertész Krisztina",
-    tipus: "férfi/női",
-    leírás: "mi alapján tervezte meg",
-    tovabbiKepek: [
-      {
-        eleres: "kep2.jpg",
-      },
-    ],
-    ar: "Nem eladó",
-    modellNeve: "XY",
-    ruhaoldalEleresiut: "",
-  },
-  {
-    kepCim: "kep cím",
-    eleres: "kep.jpg",
-    keszito: "Mészáros Mercédesz Beatrix",
-    tipus: "férfi/női",
-    leírás: "mi alapján tervezte meg",
-    tovabbiKepek: [
-      {
-        eleres: "kep2.jpg",
-      },
-    ],
-    ar: "Nem eladó",
-    modellNeve: "XY",
-    ruhaoldalEleresiut: "",
-  },
-  {
-    kepCim: "kep cím",
-    eleres: "kep.jpg",
-    keszito: "Nagy Dominika Anna",
-    tipus: "férfi/női",
-    leírás: "mi alapján tervezte meg",
-    tovabbiKepek: [
-      {
-        eleres: "kep2.jpg",
-      },
-    ],
-    ar: "Nem eladó",
-    modellNeve: "XY",
-    ruhaoldalEleresiut: "",
-  },
-  {
-    kepCim: "kep cím",
-    eleres: "kep.jpg",
-    keszito: "Nagy Flóra",
-    tipus: "férfi/női",
-    leírás: "mi alapján tervezte meg",
-    tovabbiKepek: [
-      {
-        eleres: "kep2.jpg",
-      },
-    ],
-    ar: "Nem eladó",
-    modellNeve: "XY",
-    ruhaoldalEleresiut: "",
-  },
-  {
-    kepCim: "kep cím",
-    eleres: "kep.jpg",
-    keszito: "Nagy Hanna Napsugár",
-    tipus: "férfi/női",
-    leírás: "mi alapján tervezte meg",
-    tovabbiKepek: [
-      {
-        eleres: "kep2.jpg",
-      },
-    ],
-    ar: "Nem eladó",
-    modellNeve: "XY",
-    ruhaoldalEleresiut: "",
-  },
-  {
-    kepCim: "kep cím",
-    eleres: "kep.jpg",
-    keszito: "Novák Nóra",
-    tipus: "férfi/női",
-    leírás: "mi alapján tervezte meg",
-    tovabbiKepek: [
-      {
-        eleres: "kep2.jpg",
-      },
-    ],
-    ar: "Nem eladó",
-    modellNeve: "XY",
-    ruhaoldalEleresiut: "",
-  },
-  {
-    kepCim: "kep cím",
-    eleres: "kep.jpg",
-    keszito: "Szoboszlai Bella",
-    tipus: "férfi/női",
-    leírás: "mi alapján tervezte meg",
-    tovabbiKepek: [
-      {
-        eleres: "kep2.jpg",
-      },
-    ],
-    ar: "Nem eladó",
-    modellNeve: "XY",
-    ruhaoldalEleresiut: "",
-  },
-  {
-    kepCim: "kep cím",
-    eleres: "kep.jpg",
-    keszito: "Szórád Nikolett",
-    tipus: "férfi/női",
-    leírás: "mi alapján tervezte meg",
-    tovabbiKepek: [
-      {
-        eleres: "kep2.jpg",
-      },
-    ],
-    ar: "Nem eladó",
-    modellNeve: "XY",
-    ruhaoldalEleresiut: "",
-  },
-  {
-    kepCim: "kep cím",
-    eleres: "kep.jpg",
-    keszito: "Tóth Dávid",
-    tipus: "férfi/női",
-    leírás: "mi alapján tervezte meg",
-    tovabbiKepek: [
-      {
-        eleres: "kep2.jpg",
-      },
-    ],
-    ar: "Nem eladó",
-    modellNeve: "XY",
-    ruhaoldalEleresiut: "",
-  },
-  {
-    kepCim: "kep cím",
-   eleres: "kep.jpg",
-   keszito: "Wertheim Loretta",
-   tipus: "férfi/női",
-   leírás: "mi alapján tervezte meg",
-   tovabbiKepek: [{
-       eleres: "kep2.jpg"
-   }],
-   ar: "Nem eladó",
-   modellNeve: "XY",
-   ruhaoldalEleresiut: ""
- }
-];
+ const galeriaAdatok = [
+   {
+//     kepCim: "kep cím",
+//     eleres: "../kepek/lanyokKepei/",
+//     keszito: "Árokszállási Renáta Andrea",
+//     tipus: "női",
+//     leírás: "mi alapján tervezte meg",
+//     tovabbiKepek: [
+//       {
+//         eleres: "kep2.jpg",
+//       },
+//     ],
+//     ar: "Nem eladó",
+//     modellNeve: "XY",
+//     ruhaoldalEleresiut: "",
+//   },
+//   {
+//     kepCim: "kep cím",
+//     eleres: "kep.jpg",
+//     keszito: "Bajusz Gyöngyike",
+//     tipus: "férfi/női",
+//     leírás: "mi alapján tervezte meg",
+//     tovabbiKepek: [
+//       {
+//         eleres: "kep2.jpg",
+//       },
+//     ],
+//     ar: "Nem eladó",
+//     modellNeve: "XY",
+//     ruhaoldalEleresiut: "",
+//   },
+//   {
+//     kepCim: "kep cím",
+//     eleres: "kep.jpg",
+//     keszito: "Balla Robin",
+//     tipus: "férfi/női",
+//     leírás: "mi alapján tervezte meg",
+//     tovabbiKepek: [
+//       {
+//         eleres: "kep2.jpg",
+//       },
+//     ],
+//     ar: "Nem eladó",
+//     modellNeve: "XY",
+//     ruhaoldalEleresiut: "",
+//   },
+//   {
+//     kepCim: "kep cím",
+//     eleres: "kep.jpg",
+//     keszito: "Bartha Emma Zsuzsanna",
+//     tipus: "férfi/női",
+//     leírás: "mi alapján tervezte meg",
+//     tovabbiKepek: [
+//       {
+//         eleres: "kep2.jpg",
+//       },
+//     ],
+//     ar: "Nem eladó",
+//     modellNeve: "XY",
+//     ruhaoldalEleresiut: "",
+//   },
+//   {
+//     kepCim: "kep cím",
+//     eleres: "kep.jpg",
+//     keszito: "Berecz Lili Blanka",
+//     tipus: "férfi/női",
+//     leírás: "mi alapján tervezte meg",
+//     tovabbiKepek: [
+//       {
+//         eleres: "kep2.jpg",
+//       },
+//     ],
+//     ar: "Nem eladó",
+//     modellNeve: "XY",
+//     ruhaoldalEleresiut: "",
+//   },
+//   {
+//     kepCim: "kep cím",
+//     eleres: "kep.jpg",
+//     keszito: "Buzás Vanda Szonja",
+//     tipus: "férfi/női",
+//     leírás: "mi alapján tervezte meg",
+//     tovabbiKepek: [
+//       {
+//         eleres: "kep2.jpg",
+//       },
+//     ],
+//     ar: "Nem eladó",
+//     modellNeve: "XY",
+//     ruhaoldalEleresiut: "",
+//   },
+//   {
+//     kepCim: "kep cím",
+//     eleres: "kep.jpg",
+//     keszito: "Fábián Fanni",
+//     tipus: "férfi/női",
+//     leírás: "mi alapján tervezte meg",
+//     tovabbiKepek: [
+//       {
+//         eleres: "kep2.jpg",
+//       },
+//     ],
+//     ar: "Nem eladó",
+//     modellNeve: "XY",
+//     ruhaoldalEleresiut: "",
+//   },
+//   {
+//     kepCim: "kep cím",
+//     eleres: "kep.jpg",
+//     keszito: "Fazekas Anna",
+//     tipus: "férfi/női",
+//     leírás: "mi alapján tervezte meg",
+//     tovabbiKepek: [
+//       {
+//         eleres: "kep2.jpg",
+//       },
+//     ],
+//     ar: "Nem eladó",
+//     modellNeve: "XY",
+//     ruhaoldalEleresiut: "",
+//   },
+//   {
+//     kepCim: "kep cím",
+//     eleres: "kep.jpg",
+//     keszito: "Fódi Martin Levente",
+//     tipus: "férfi/női",
+//     leírás: "mi alapján tervezte meg",
+//     tovabbiKepek: [
+//       {
+//         eleres: "kep2.jpg",
+//       },
+//     ],
+//     ar: "Nem eladó",
+//     modellNeve: "XY",
+//     ruhaoldalEleresiut: "",
+//   },
+//   {
+//     kepCim: "kep cím",
+//     eleres: "kep.jpg",
+//     keszito: "Hoffmann Dóra",
+//     tipus: "férfi/női",
+//     leírás: "mi alapján tervezte meg",
+//     tovabbiKepek: [
+//       {
+//         eleres: "kep2.jpg",
+//       },
+//     ],
+//     ar: "Nem eladó",
+//     modellNeve: "XY",
+//     ruhaoldalEleresiut: "",
+//   },
+//   {
+//     kepCim: "kep cím",
+//     eleres: "kep.jpg",
+//     keszito: "Juhász Nóra",
+//     tipus: "férfi/női",
+//     leírás: "mi alapján tervezte meg",
+//     tovabbiKepek: [
+//       {
+//         eleres: "kep2.jpg",
+//       },
+//     ],
+//     ar: "Nem eladó",
+//     modellNeve: "XY",
+//     ruhaoldalEleresiut: "",
+//   },
+//   {
+//     kepCim: "kep cím",
+//     eleres: "kep.jpg",
+//     keszito: "Kertész Krisztina",
+//     tipus: "férfi/női",
+//     leírás: "mi alapján tervezte meg",
+//     tovabbiKepek: [
+//       {
+//         eleres: "kep2.jpg",
+//       },
+//     ],
+//     ar: "Nem eladó",
+//     modellNeve: "XY",
+//     ruhaoldalEleresiut: "",
+//   },
+//   {
+//     kepCim: "kep cím",
+//     eleres: "kep.jpg",
+//     keszito: "Mészáros Mercédesz Beatrix",
+//     tipus: "férfi/női",
+//     leírás: "mi alapján tervezte meg",
+//     tovabbiKepek: [
+//       {
+//         eleres: "kep2.jpg",
+//       },
+//     ],
+//     ar: "Nem eladó",
+//     modellNeve: "XY",
+//     ruhaoldalEleresiut: "",
+//   },
+//   {
+//     kepCim: "kep cím",
+//     eleres: "kep.jpg",
+//     keszito: "Nagy Dominika Anna",
+//     tipus: "férfi/női",
+//     leírás: "mi alapján tervezte meg",
+//     tovabbiKepek: [
+//       {
+//         eleres: "kep2.jpg",
+//       },
+//     ],
+//     ar: "Nem eladó",
+//     modellNeve: "XY",
+//     ruhaoldalEleresiut: "",
+//   },
+//   {
+//     kepCim: "kep cím",
+//     eleres: "kep.jpg",
+//     keszito: "Nagy Flóra",
+//     tipus: "férfi/női",
+//     leírás: "mi alapján tervezte meg",
+//     tovabbiKepek: [
+//       {
+//         eleres: "kep2.jpg",
+//       },
+//     ],
+//     ar: "Nem eladó",
+//     modellNeve: "XY",
+//     ruhaoldalEleresiut: "",
+//   },
+//   {
+//     kepCim: "kep cím",
+//     eleres: "kep.jpg",
+//     keszito: "Nagy Hanna Napsugár",
+//     tipus: "férfi/női",
+//     leírás: "mi alapján tervezte meg",
+//     tovabbiKepek: [
+//       {
+//         eleres: "kep2.jpg",
+//       },
+//     ],
+//     ar: "Nem eladó",
+//     modellNeve: "XY",
+//     ruhaoldalEleresiut: "",
+//   },
+//   {
+//     kepCim: "kep cím",
+//     eleres: "kep.jpg",
+//     keszito: "Novák Nóra",
+//     tipus: "férfi/női",
+//     leírás: "mi alapján tervezte meg",
+//     tovabbiKepek: [
+//       {
+//         eleres: "kep2.jpg",
+//       },
+//     ],
+//     ar: "Nem eladó",
+//     modellNeve: "XY",
+//     ruhaoldalEleresiut: "",
+//   },
+//   {
+//     kepCim: "kep cím",
+//     eleres: "kep.jpg",
+//     keszito: "Szoboszlai Bella",
+//     tipus: "férfi/női",
+//     leírás: "mi alapján tervezte meg",
+//     tovabbiKepek: [
+//       {
+//         eleres: "kep2.jpg",
+//       },
+//     ],
+//     ar: "Nem eladó",
+//     modellNeve: "XY",
+//     ruhaoldalEleresiut: "",
+//   },
+//   {
+//     kepCim: "kep cím",
+//     eleres: "kep.jpg",
+//     keszito: "Szórád Nikolett",
+//     tipus: "férfi/női",
+//     leírás: "mi alapján tervezte meg",
+//     tovabbiKepek: [
+//       {
+//         eleres: "kep2.jpg",
+//       },
+//     ],
+//     ar: "Nem eladó",
+//     modellNeve: "XY",
+//     ruhaoldalEleresiut: "",
+//   },
+//   {
+//     kepCim: "kep cím",
+//     eleres: "kep.jpg",
+//     keszito: "Tóth Dávid",
+//     tipus: "férfi/női",
+//     leírás: "mi alapján tervezte meg",
+//     tovabbiKepek: [
+//       {
+//         eleres: "kep2.jpg",
+//       },
+//     ],
+//     ar: "Nem eladó",
+//     modellNeve: "XY",
+//     ruhaoldalEleresiut: "",
+//   },
+//   {
+//     kepCim: "kep cím",
+//    eleres: "kep.jpg",
+//    keszito: "Wertheim Loretta",
+//    tipus: "férfi/női",
+//    leírás: "mi alapján tervezte meg",
+//    tovabbiKepek: [{
+//        eleres: "kep2.jpg"
+//    }],
+//    ar: "Nem eladó",
+//    modellNeve: "XY",
+//    ruhaoldalEleresiut: ""
+//  }
+// ];

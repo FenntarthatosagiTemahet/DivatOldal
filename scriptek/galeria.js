b3677f3d32bf5314c7c807c8815a9cb8a0d25269
WeinbergerPeti <weinbergerpeti@gmail.com>
2022-05-24 22:39:11 +0200

    oldal javításai

--- scriptek/galeria.js
+++ scriptek/galeria.js
@@ -18,13 +18,13 @@ function CLASS(elem) {
 
 const zeroWasteKepek=[];
 const ingKepek=[];
-const szoveg=[];
 
 function init() {
   $("footer").innerHTML = "&copy Készítette: Weinberger Péter";
   tombBeolvasas(ingKepek, "UjrahasznositottIng");
   tombBeolvasas(zeroWasteKepek, "zeroWaste");
   valaszt();
+  szovegKiiras(0);
 }
 
 function valaszt(){
@@ -33,11 +33,13 @@ function valaszt(){
   tomb.forEach((elem)=>{
     elem.addEventListener("click", function(){
       melyik = event.target.innerHTML;
-      if(melyik === "Zero waste"){
+      if(melyik === "Zero waste kollekció"){
         $("#zero").addEventListener("click", galeriaFeltoltAdatokkal(zeroWasteKepek));
+        szovegKiiras(0);
       }
-      else if(melyik ==="Újrahasznosított ingek"){
+      else if(melyik ==="Kreatív kollekció"){
         $("#ing").addEventListener("click",galeriaFeltoltAdatokkal(ingKepek));
+        szovegKiiras(1);
       }
     })
   })
@@ -65,7 +67,7 @@ function galeriaFeltoltAdatokkal(tomb)
   {
       txt+=`<div id="${i}"><a href="ruha.html"><img loading="lazy" id="${i}" src="${tomb[i].foKepEleresiUt}" alt="kép">
           <p><span id="${i}">${tomb[i].keszito}</span></p>
-          <p>(<span>${tomb[i].modellNeve}</span>)</p></a></div>`;
+          <p id="modell">modell: <span>${tomb[i].modellNeve}</span></p></a></div>`;
 
       asideTxt+=`<p><a id="${i}" href="ruha.html">${tomb[i].keszito}</a></p>`;
   }
@@ -93,3 +95,26 @@ function galeriaFeltoltAdatokkal(tomb)
     });
   }
 }
+ const szoveg=[
+
+     {
+     szovegCim: "A zero waste kollekció:",
+     szovegLeiras: "<p>Egy jól tervezett folyamat lényege, hogy ne keletkezzen szemét, legfeljebb hulladék. A természetben az anyagok folyamatos átalakuláson mennek keresztül, de sohasem kerülnek ki egy zárt körforgásból. Vagyis körkörösen, a lehető leghatékonyabban újrahasznosítható erőforrások. Ezzel szemben az átgondolatlan termelési folyamatok felesleget eredményeznek. Ez a szemét. Teljesen mindegy, hogy a szemét elpazarolt alapanyag, energia, vagy humán tőke.<br>A hulladék nélküli öltözékek elkészítése magas szintű szabászati és kivitelezési ismereteket feltételez, ami korlátozta volna a lehetőségeket. Ezért a hulladék mentesség bemutatására három tervezési és kivitelezési koncepciót választottunk:</p><ul><li>hulladékmentes szabászati és kivitelezési megoldásokkal készített öltözékek</li><li>maradék anyagok felhasználásával készített öltözékek</li><li>upcycle koncepcióval készített öltözékek</li></ul><p>Ezekkel az eljárásokkal azok számára is adhatnak a diákok kreatív ötleteket, akik tudnak varrni és azoknak is, akik most kapnak kedvet ahhoz, hogy megtanulják.</p>"
+   },
+   {
+     szovegCim: "A kreatív kísérletek kollekció:",
+     szovegLeiras: "<p>A kreatív kísérletek kollekció bepillantást enged a tervezési folyamat egyik fázisába, ahol a diákok kötöttségek nélkül, a formákkal szabadon kísérletezve alkothatták meg az elképzeléseiket régi férfiingeket használva alapanyagként.<br>A feladat az ing alkotóelemeire bontása, a kapott alkatrészek újrakomponálása volt.<br>A bemutatásra kerülő modellek a tervezési folyamat egy pillanatfelvételét testesítik meg, a kibontakozó ötletek egy-egy lehetséges irányát mutatják meg a számtalan lehetőség közül, amelyeket továbbfejlesztve születnek majd meg a végső öltözékek.</p>"
+   }
+]
+
+function szovegKiiras(index) 
+{
+  var szovegTxt="";
+    szovegTxt+=`<h2>${szoveg[index].szovegCim}</h2>${szoveg[index].szovegLeiras}`;
+  
+  szovegTxt+="";
+  console.log(szovegTxt);
+  $("#zeroSzoveg").innerHTML=szovegTxt;  
+  console.log(szoveg);
+}
+ 
--- scriptek/ruha.js
+++ scriptek/ruha.js
@@ -42,7 +42,7 @@ function feltolt()
         <p><span id="keszitoNeve">${ruhaKepekTomb[i].keszito}</span></p>
         <p><span>${ruhaKepekTomb[i].tipus}</span></p>
         <p>${ruhaKepekTomb[i].leiras}</p>
-        <p>Modell neve: <span>${ruhaKepekTomb[i].modellNeve}</p></span><div>`;
+        <p>Modell: <span>${ruhaKepekTomb[i].modellNeve}</p></span><div>`;
         $("h1").innerHTML=ruhaKepekTomb[i].keszito;
     }
 
